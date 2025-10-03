692238f1246d6c16c5453cef3fc9cbe3e442929b
tezzy <zacharyeverett.00@gmail.com>
2025-10-04 01:17:09 +1000

    still working on tauri integration, questioning if a rust webserver that frontend connects to would be a better option

--- renderer/src-tauri/src/lib.rs
+++ renderer/src-tauri/src/lib.rs
@@ -1,15 +1,27 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
 use chess_game;
+use serde::Serialize;
 use std::sync::Arc;
 use std::sync::Mutex;
 use tauri::State;
 
-
+#[derive(Serialize)]
+struct BoardSlot{
+    piece: String,
+    team: bool
+}
 #[tauri::command]
-fn get_board(state: State<Arc<Mutex<chess_game::Board>>>) -> Vec<[chess_game::Piece; 8]>{
+fn get_board(state: State<Arc<Mutex<chess_game::Board>>>) { 
     let board = state.lock().unwrap();
-    return board.tiles.clone().to_vec();
+    for row in &board.tiles {
+        for piece in row {
+            let board_slot = BoardSlot {
+               piece: String::from("Empty"),
+               team: true,
+            };
+        }
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
